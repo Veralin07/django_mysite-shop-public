1866d8eaed09882e9ca0d2534838ba8e5347384a
Veralin_FZ <116857711+Veralin07@users.noreply.github.com>
2025-10-14 13:40:55 +0800

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-tni1bn$0&e@bj)wr1zkv&n^3bo!l3t1)5u&d%f(d+$a97(%0xg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['5.129.211.114', '77.95.206.205', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['77.95.206.205', 'localhost', '127.0.0.1']
 
 # Application definition
 
@@ -202,3 +202,4 @@ SPECTACULAR_SETTINGS = {
     'SERVE_INCLUDE_SCHEMA': False,
 
 }
+
